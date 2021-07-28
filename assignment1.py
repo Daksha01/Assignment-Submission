5ae2124a0e415b41cbf8b793ed08dbb0831fd09c
Daksha01 <88086864+Daksha01@users.noreply.github.com>
2021-07-28 16:02:59 +0530

    Day 1 Assignment Submitted

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+# List of Integers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  
+# Sorting list of Integers
+numbers.sort(reverse=True)
+  
+print(numbers)
+  
+# List of Floating point numbers
+decimalnumber = [2.00, 2.01, 2.02, 2.03, 5.68]
+  
+# Sorting list of Floating point numbers
+decimalnumber.sort(reverse=True)
+  
+print(decimalnumber)
+  
+# List of strings
+words = ["My", "name", "is", "Daksha"]
+  
+# Sorting list of strings
+words.sort(reverse=True)
+  
+print(words)
